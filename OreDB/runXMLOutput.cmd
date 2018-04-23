04c3f82375abe2f61ae58f6c3764caf3ea9d08a1
rkapl123 <rolandk@kapl.org>
2018-04-24 00:36:28 +0200

    repaired runXMLOutput.cmd

--- OreDB/runXMLOutput.cmd
+++ OreDB/runXMLOutput.cmd
@@ -7,10 +7,10 @@ sqlcmd -S LENOVO-PC -d ORE -E -Q "set nocount on;select XMLdata from PricingEngi
 sqlcmd -S LENOVO-PC -d ORE -E -Q "set nocount on;select XMLdata from TodaysmarketSelection where GroupingId = 'ExampleInput'" -y0 -o OREDB\todaysmarket.xml
 sqlcmd -S LENOVO-PC -d ORE -E -Q "set nocount on;select XMLdata from OreParametersSelection where OreConfigId = 'Example_2'" -y0 -o OREDB\ore.xml
 sqlcmd -S LENOVO-PC -d ORE -E -Q "set nocount on;select XMLdata from NettingSetSelection where GroupingId = 'Example_2'" -y0 -o OREDB\netting.xml
-sqlcmd -S LENOVO-PC -d ORE -E -Q "set nocount on;select XMLdata from PortfolioSelection where GroupingId = 'Example_2'" -y0 -o OREDB\portfolio.xml
+sqlcmd -S LENOVO-PC -d ORE -E -Q "set nocount on;select XMLdata from PortfolioSelection where AdditionalId = 'Example_2'" -y0 -o OREDB\portfolio.xml
 sqlcmd -S LENOVO-PC -d ORE -E -Q "set nocount on;select XMLdata from SimulationSelection where Id = 'Example_2'" -y0 -o OREDB\simulation.xml
 sqlcmd -S LENOVO-PC -d ORE -E -Q "set nocount on;select XMLdata from SensitivityanalysisSelection where Id = 'Example_15'" -y0 -o OREDB\sensitivity.xml
-sqlcmd -S LENOVO-PC -d ORE -E -Q "set nocount on;select XMLdata from StresstestSelection where Id = 'Example_15'" -y0 -o OREDB\stresstest.xml
+sqlcmd -S LENOVO-PC -d ORE -E -Q "set nocount on;select XMLdata from StresstestSelection where GroupingId = 'Example_15'" -y0 -o OREDB\stresstest.xml
 sqlcmd -S LENOVO-PC -d ORE -E -Q "set nocount on;SELECT f.FixingDate,fd.FixingIndex,f.IndexValue FROM FixingData f INNER JOIN FixingDataDefinitions fd ON f.IndexId = fd.IndexId ORDER BY f.FixingDate" -y0 -o OREDB\fixingdata.txt
 sqlcmd -S LENOVO-PC -d ORE -E -Q "set nocount on;SELECT m.QuoteDate,md.Quote,m.QuoteValue FROM MarketData m INNER JOIN MarketDataDefinitions md ON m.QuoteId = md.QuoteId" -y0 -o OREDB\marketdata.txt
 sqlcmd -S LENOVO-PC -d ORE -E -Q "set nocount on;SELECT QuoteId1,QuoteId2,QuoteValue FROM CovarianceData" -y0 -o OREDB\covariancedata.txt
