046d9d1c6905b63f00214c2e082d606af783fee4
Kapl Roland <roland.kapl@oebfa.at>
2018-12-01 18:55:28 +0000

    remove data sql files from versioning except manually edited ore_types

--- OreDB/Data/ore_typesParties.sql
+++
@@ -1,7 +0,0 @@
-use ORE;
-
-INSERT TypesParties (value) VALUES ('CPTY_C');
-INSERT TypesParties (value) VALUES ('BANK');
-INSERT TypesParties (value) VALUES ('BOND_YIELD_EUR_OVER_OIS');
-INSERT TypesParties (value) VALUES ('CPTY_B');
-INSERT TypesParties (value) VALUES ('CPTY_A');
